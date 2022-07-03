767608388dac62903ea8d3fc4af9256e5bd2b38f
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2022-07-04 00:11:00 +0300

    - basic dashboard page complete

---
+++ src/components/Dashboard/MonthCard.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import {
+    createStyles,
+    ThemeIcon,
+    Progress,
+    Text,
+    Group,
+    Badge,
+    Paper,
+} from '@mantine/core'
+import { Swimming, Wallet } from 'tabler-icons-react'
+
+const ICON_SIZE = 60
+
+const useStyles = createStyles((theme) => ({
+    card: {
+        position: 'relative',
+        overflow: 'visible',
+        padding: theme.spacing.xl,
+        paddingTop: theme.spacing.xl * 1.5 + ICON_SIZE / 3,
+    },
+
+    icon: {
+        position: 'absolute',
+        top: -ICON_SIZE / 3,
+        left: `calc(50% - ${ICON_SIZE / 2}px)`,
+    },
+
+    title: {
+        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+        lineHeight: 1,
+    },
+}))
+
+export default function MonthCard() {
+    const { classes } = useStyles()
+
+    return (
+        <Paper
+            radius="md"
+            withBorder
+            className={classes.card}
+            mb={ICON_SIZE / 3}
+        >
+            <ThemeIcon
+                className={classes.icon}
+                size={ICON_SIZE}
+                radius={ICON_SIZE}
+            >
+                <Wallet size={34} />
+            </ThemeIcon>
+
+            <Text align="center" weight={700} className={classes.title} mb="sm">
+                Savings challenge
+            </Text>
+            <Text color="dimmed" align="center" size="sm">
+                32K / week
+            </Text>
+
+            <Group position="apart" mt="xs">
+                <Text size="sm" color="dimmed">
+                    Progress
+                </Text>
+                <Text size="sm" color="dimmed">
+                    62%
+                </Text>
+            </Group>
+
+            <Progress value={62} mt={5} />
+
+            <Group position="apart" mt="md">
+                <Text size="sm">20 / 36k</Text>
+                <Badge size="sm">4 days left</Badge>
+            </Group>
+        </Paper>
+    )
+}
---
+++ src/components/Dashboard/Stats.tsx
@@ -0,0 +1,111 @@
+import React, { ReactNode } from 'react'
+import { createStyles, Group, Paper, SimpleGrid, Text } from '@mantine/core'
+import {
+    UserPlus,
+    Discount2,
+    Receipt2,
+    Coin,
+    ArrowUpRight,
+    ArrowDownRight,
+    Wallet,
+    Receipt,
+} from 'tabler-icons-react'
+
+const useStyles = createStyles((theme) => ({
+    root: {
+        paddingTop: theme.spacing.xl * 1.5,
+        paddingBottom: theme.spacing.xl * 1.5,
+    },
+
+    value: {
+        fontSize: 24,
+        fontWeight: 700,
+        lineHeight: 1,
+    },
+
+    diff: {
+        lineHeight: 1,
+        display: 'flex',
+        alignItems: 'center',
+    },
+
+    icon: {
+        color:
+            theme.colorScheme === 'dark'
+                ? theme.colors.dark[3]
+                : theme.colors.gray[4],
+    },
+
+    title: {
+        fontWeight: 700,
+        textTransform: 'uppercase',
+    },
+}))
+
+const icons = {
+    wallet: Wallet,
+    receipt2: Receipt2,
+    receipt: Receipt,
+    coin: Coin,
+}
+
+interface StatsGridProps {
+    data: {
+        title: string
+        icon: string
+        value: string
+        diff: number
+    }[]
+}
+
+export default function StatsSection({ data }: StatsGridProps) {
+    const { classes } = useStyles()
+    const stats = data.map((stat) => {
+        // const Icon = icons[stat.icon]
+
+        // @ts-ignore
+        const Icon = icons[stat.icon]
+        const DiffIcon = stat.diff > 0 ? ArrowUpRight : ArrowDownRight
+
+        return (
+            <Paper withBorder p="md" radius="md" key={stat.title}>
+                <Group position="apart">
+                    <Text size="xs" color="dimmed" className={classes.title}>
+                        {stat.title}
+                    </Text>
+                    <Icon className={classes.icon} size={22} />
+                </Group>
+
+                <Group align="flex-end" spacing="xs" mt={25}>
+                    <Text className={classes.value}>{stat.value}</Text>
+                    <Text
+                        color={stat.diff > 0 ? 'teal' : 'red'}
+                        size="sm"
+                        weight={500}
+                        className={classes.diff}
+                    >
+                        <span>{stat.diff}%</span>
+                        <DiffIcon size={16} />
+                    </Text>
+                </Group>
+
+                <Text size="xs" color="dimmed" mt={7}>
+                    Compared to previous month
+                </Text>
+            </Paper>
+        )
+    })
+    return (
+        <div className={classes.root}>
+            <SimpleGrid
+                cols={4}
+                breakpoints={[
+                    { maxWidth: 'md', cols: 2 },
+                    { maxWidth: 'xs', cols: 1 },
+                ]}
+            >
+                {stats}
+            </SimpleGrid>
+        </div>
+    )
+}
